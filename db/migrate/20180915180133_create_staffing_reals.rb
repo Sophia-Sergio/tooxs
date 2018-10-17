a96c92854a0bcd23fef73b048bcf637013f7ba0f
Sophia-Sergio <san.storres@gmail.com>
2018-10-17 18:32:14 -0300

    Alter migration create table if doesn't exists table

--- db/migrate/20180915180133_create_staffing_reals.rb
+++ db/migrate/20180915180133_create_staffing_reals.rb
@@ -1,5 +1,7 @@
 class CreateStaffingReals < ActiveRecord::Migration[5.1]
   def change
+    return if table_exists?(:staffing_reals)
+
     create_table :staffing_reals do |t|
       t.references :department, foreign_key: true
       t.integer :year
