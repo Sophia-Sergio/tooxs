4176990fa7643850b0c053f7b0825434aee8de11
Sophia-Sergio <san.storres@gmail.com>
2018-10-17 13:58:21 -0300

    Remove unused files, delete some extra blank lines

--- app/helpers/return_cases_helper.rb
+++
@@ -1,2 +0,0 @@
-module ReturnCasesHelper
-end
--- db/migrate/20180508162642_fix_column_name_to_data_case.rb
+++
@@ -1,5 +0,0 @@
-class FixColumnNameToDataCase < ActiveRecord::Migration[5.1]
-  def change
-  	rename_column :data_cases, :luch_in, :lunch_in
-  end
-end
