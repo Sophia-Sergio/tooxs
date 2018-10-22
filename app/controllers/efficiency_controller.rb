7c1db9842101f6904a6d3c518fc3c62e0e194fb4
erwin254 <erwin254@gmail.com>
2018-10-21 21:17:42 -0300

    update EfficiencyController

--- app/controllers/efficiency_controller.rb
+++ app/controllers/efficiency_controller.rb
@@ -1,19 +1,14 @@
 class EfficiencyController < ApplicationController
 	def index	
-    	@stores       = Store.all.order(:id)
-    	@departments  = Department.all
-    	@search = ""
-        if params[:month]
-        	@month = params[:month]
-        else
-        	@month = timeNow.strftime("%m").to_i 
-        end
+		@stores       = Store.all.order(:id)
+		@departments  = Department.all.order(:id)
+		@search = ""
+		@month = params[:month] || timeNow.strftime("%m").to_i
 	end
 
-    def report
-        excesoReal = matrix_calc(@prod_obj, @totalMonth, dotReal)
-        totalHour = (@realMonth.sum / @prod_obj)
-        @margin_adjustment = (1 - ((excesoReal[:exceso] + excesoReal[:faltante]) / totalHour)).round(4) * 100 
-    end
-
+	def report
+		excesoReal = matrix_calc(@prod_obj, @totalMonth, dotReal)
+		totalHour = (@realMonth.sum / @prod_obj)
+		@margin_adjustment = (1 - ((excesoReal[:exceso] + excesoReal[:faltante]) / totalHour)).round(4) * 100 
+	end
 end
