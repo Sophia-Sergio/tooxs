6bdee1282c033e3be64cd29c7ff7790040fdfa1b
Sophia-Sergio <san.storres@gmail.com>
2018-10-26 12:28:40 -0300

    Add new endpoint

---
+++ app/controllers/market/dashboard_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# Main dashboard controller
+module Market
+  # Main dashboard controller
+  class DashboardController < ApplicationController
+    def index() end
+  end
+end
