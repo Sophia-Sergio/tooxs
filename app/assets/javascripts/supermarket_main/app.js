cc933017e931d1af6a29d7b2cb4f78c994008a84
gorylactico <adrian.miranda.r@gmail.com>
2018-10-31 00:40:51 -0300

    inserting charts in their respective boxes

--- app/assets/javascripts/supermarket_main/app.js
+++ app/assets/javascripts/supermarket_main/app.js
@@ -1,18 +1,36 @@
 window.onload = function() {
-    new Chart(document.getElementById("myChart").getContext('2d'), {
+    new Chart(document.getElementById("myChart1").getContext('2d'), {
         type: 'pie',
         data: {
-        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
+        labels: ["Bebidas", "Verduras", "Cereales", "Quesos y Fiambres"],
         datasets: [{
             label: "Population (millions)",
-            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
             data: [2478,5267,734,784,433]
         }]
         },
         options: {
         title: {
             display: true,
-            text: 'Predicted world population (millions) in 2050'
+            // text: 'Predicted world population (millions) in 2050'
+        }
+        }
+    });
+
+    new Chart(document.getElementById("myChart2").getContext('2d'), {
+        type: 'pie',
+        data: {
+        labels: ["Turno 45hrs", "Turno 36hrs"],
+        datasets: [{
+            label: "Population (millions)",
+            backgroundColor: ["#e8c3b9","#c45850"],
+            data: [784,433]
+        }]
+        },
+        options: {
+        title: {
+            display: true,
+            // text: 'Predicted world population (millions) in 2050'
         }
         }
     });
