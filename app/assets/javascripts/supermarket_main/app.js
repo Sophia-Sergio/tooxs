e55bf53b018c090ba48a70d05f825cb2415e0a60
gorylactico <adrian.miranda.r@gmail.com>
2018-10-30 22:52:46 -0300

    insertion test chart

--- app/assets/javascripts/supermarket_main/app.js
+++ app/assets/javascripts/supermarket_main/app.js
@@ -1,38 +1,19 @@
-$(document).ready(function () {
-    var ctx = document.getElementById("myChart");
-var myChart = new Chart(ctx, {
-    type: 'pie',
-    data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+window.onload = function() {
+    new Chart(document.getElementById("myChart").getContext('2d'), {
+        type: 'pie',
+        data: {
+        labels: ["Africa", "Asia", "Europe", "Latin America", "North America"],
         datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
+            label: "Population (millions)",
+            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
+            data: [2478,5267,734,784,433]
         }]
-    },
-    options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true
-                }
-            }]
+        },
+        options: {
+        title: {
+            display: true,
+            text: 'Predicted world population (millions) in 2050'
         }
-    }
-});
+        }
+    });
+  };
