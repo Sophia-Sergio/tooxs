028b3198a27bbe33126346892bffc2727630ad9a
gorylactico <adrian.miranda.r@gmail.com>
2018-11-02 02:23:32 -0300

    creatio of a new table for plannig, insertion of horizontal bar chart, modification of charts in relation to color and amount of measured data

--- app/assets/javascripts/supermarket_main/app.js
+++ app/assets/javascripts/supermarket_main/app.js
@@ -2,17 +2,17 @@ window.onload = function() {
     new Chart(document.getElementById("myChart1").getContext('2d'), {
         type: 'pie',
         data: {
-        labels: ["Bebidas", "Verduras", "Cereales", "Quesos y Fiambres"],
+        labels: ["76.6% de cumplimiento"],
         datasets: [{
-            label: "Population (millions)",
-            backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9"],
-            data: [2478,5267,734,784,433]
+            label: "cumplimiento",
+            backgroundColor: ["#6b6ef9","#c8c7cd"],
+            data: [1500,433]
         }]
         },
         options: {
         title: {
             display: true,
-            // text: 'Predicted world population (millions) in 2050'
+            text: 'Cumplimiento de plan'
         }
         }
     });
@@ -20,18 +20,40 @@ window.onload = function() {
     new Chart(document.getElementById("myChart2").getContext('2d'), {
         type: 'pie',
         data: {
-        labels: ["Turno 45hrs", "Turno 36hrs"],
+        labels: ["67.02% Productividad objetiva xxx unidades/hora"],
         datasets: [{
-            label: "Population (millions)",
-            backgroundColor: ["#e8c3b9","#c45850"],
+            label: "unidades",
+            backgroundColor: ["#ff0076","#c8c7cd"],
             data: [784,433]
         }]
         },
         options: {
         title: {
             display: true,
-            // text: 'Predicted world population (millions) in 2050'
+            text: 'Eficiencia'
         }
         }
     });
+
+    // new Chart(document.getElementById("myChart3"), {
+    //     type: 'horizontalBar',
+    //     data: {
+    //       labels: ["Africa",],
+    //       datasets: [
+    //         {
+    //           label: "Turnos",
+    //           backgroundColor: ["#3e95cd",],
+    //           data: [2478,5267,734,784,433]
+    //         }
+    //       ]
+    //     },
+    //     options: {
+    //       legend: { display: false },
+    //       title: {
+    //         display: true,
+    //         text: 'Turnos Cubiertos'
+    //       }
+    //     }
+    // });
+    
   };
