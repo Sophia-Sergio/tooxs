010a2fa20f2f545e7f5dc5cf901a23963494233e
Sophia-Sergio <san.storres@gmail.com>
2018-11-14 11:10:58 -0300

    refactor: dry - identation

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,42 +1,18 @@
 class DashboardController < ApplicationController
   include DemoParameters
+  before_action :index_data, :index
 
-	def index
-    @month = params[:month] || demo_data[:date].month
-    @store = demo_data[:store]
-    @department  = demo_data[:department]
-    @search      = demo_data[:search]
-    @stores      = Store.where(id: demo_data[:store])
-    @departments = Department.where(id: demo_data[:department])
-    @year  = Date.today.year
-    # dayNow = day_now_charged
-    turnos = Array.new(12, 0)
-
-    dataCase = DataCase.where(dep_num: demo_data[:department], month: @month).first
-
-    binding.pry
-
-    summaryCaseOut = SummaryCase.where(id_case: dataCase.id_case, type_io: 'out').first if dataCase
-
-    if summaryCaseOut
-      @margin_adjustment = summaryCaseOut.margin_adjustment.to_f
-      @prod_obj = dataCase.prod_obj.to_f
-      @prod_real = setNum((@prod_obj * @margin_adjustment) / 100)
-    else
-      @prod_obj = 0
-      @prod_real = 0
-      @margin_adjustment = 0
-    end
-
-		#turnos cubiertos
-    if dataCase
-      shifts_covered_data = shifts_covered(dataCase.id_case, @department, @store)
+  def index
+    #turnos cubiertos
+    if @data_case
+      shifts_covered_data = shifts_covered(@data_case.id, @department.id, @store.id)
       turnosOpTotal = shifts_covered_data[:turnosOpTotal]
       @turnosEntrada = shifts_covered_data[:turnosEntrada]
       @turnosOptimizados = shifts_covered_data[:turnosOptimizados]
     end
 
-    @sellers = Seller.where(department: @department, store:  @store)
+    @sellers = @store.sellers.by_department(@department)
+
     @setSellers = []
     @planVentaTotal = 0
     @ventaTotal = 0
@@ -47,7 +23,6 @@ class DashboardController < ApplicationController
       seller_plan.flatten.each_with_index do |plan_, index|
         plan += plan_[index + 1][:seller_plan_per_day].sum
       end
-
       sale = sale_real_per_seller(seller, @year, @month)
 			cumplimiento = ((sale.to_f / plan.to_f) * 100).round(2)
       @setSellers << { :seller => seller,
@@ -59,48 +34,46 @@ class DashboardController < ApplicationController
     end
 
 		@turnos_cubiertos = []
-		if @turnosOptimizados
-			if @turnosOptimizados.sum != 0
-				@turnos_cubiertos = { :texto => " #{turnosOpTotal} de #{@turnosOptimizados.sum}", :porcentaje => ( turnosOpTotal* 100 / @turnosOptimizados.sum).round }
-			else
-				@turnos_cubiertos = { :texto => " #{turnosOpTotal} de #{@turnosOptimizados.sum}", :porcentaje => 0 }
-			end
-    end
-
-		# calcular cumplimiento del plan
-		ventaTotal = SaleBySeller.where(month: @month, department: @department, year: @year).sum("sale").to_f
-		if summaryCaseOut
-			planVenta = summaryCaseOut.sale_plan.to_f
-      @cumplimientoPlan = (@ventaTotal / @planVentaTotal * 100).round(1)
-		else
-			planVenta = 0
-			@cumplimientoPlan = 0
-		end
-		#obtener productividad real
-		dotReal = dotacion_real(@department, @month, @year)
-
-    #obtener ventas reales del mes
-    sale_reals = SaleReal.where(department_id: @department, store_id: @store, year: @year, month: @month)
-    @realMonth = []
-		@totalMonth = []
-		@contReal = 0
-
-    if dotReal.length > 0
-      sale_reals.each do |sale|
-        totalRealDay = sale[:nine]+sale[:ten]+sale[:eleven]+sale[:twelve]+sale[:thirteen]+sale[:fourteen]+sale[:fifteen]+sale[:sixteen]+sale[:seventeen]+sale[:eighteen]+sale[:nineteen]+sale[:twenty]+sale[:twenty_one]+sale[:twenty_two]+sale[:twenty_three]+sale[:twenty_four]
-        @realMonth  << totalRealDay
-        @totalMonth << (totalRealDay.to_f / dotReal[@contReal]).round
-        @contReal += 1
+    if @turnosOptimizados
+      if @turnosOptimizados.sum != 0
+        @turnos_cubiertos = { :texto => " #{turnosOpTotal} de #{@turnosOptimizados.sum}", :porcentaje => ( turnosOpTotal* 100 / @turnosOptimizados.sum).round }
+      else
+        @turnos_cubiertos = { :texto => " #{turnosOpTotal} de #{@turnosOptimizados.sum}", :porcentaje => 0 }
       end
-      excesoReal = matrix_calc(@prod_obj, @totalMonth, dotReal)
-      totalHour = (@realMonth.sum / @prod_obj)
-      @margin_adjustment = (1 - ((excesoReal[:exceso] + excesoReal[:faltante]) / totalHour)).round(4) * 100
-			@prod_real = (@realMonth.sum / dotReal.sum).round
     end
+
+    @cumplimientoPlan = (@ventaTotal / @planVentaTotal * 100).round(1)
+
+    excesoReal = matrix_calc(@target_productivity, @average_sale_by_day, @staff_by_day)
+    totalHour = (@sales_by_day.sum / @target_productivity)
+    # eficiencia
+    @efficiency = (1 - ((excesoReal[:exceso] + excesoReal[:faltante]) / totalHour)).round(2) * 100
 	end
 
 	def sale_real_per_seller(seller,year,month)
-      @dep   = seller.department.id
-      SaleBySeller.where(month: month, seller: seller.id, department: @dep, year: year).sum("sale")
+    @dep   = seller.department.id
+    SellerSale.where(month: month, seller: seller, department: @dep, year: year).sum("sale")
+  end
+
+  private
+
+  def index_data
+    @year = params[:year] ||= demo_data[:date].year
+    @month = params[:month] ||= demo_data[:date].month
+    store = params[:store] ||= demo_data[:store]
+    department = params[:department] ||= demo_data[:department]
+    @store = Store.find(store)
+    @department = Department.find(department)
+    @staff_by_day = StaffingReal.staff_by_day(filter_params)
+    @sales_by_day = @store.sale_reals.sales_by_day(@department, @month)
+    @average_sale_by_day = @sales_by_day.zip(@staff_by_day).map { |sale_staff| (sale_staff[0] / sale_staff[1].to_f).round(2) }
+    @real_productivity = (@sales_by_day.sum / @staff_by_day.sum).round(2)
+    @data_case = DataCase.find_case(filter_params)
+    @summary_case_out = @data_case.summary_case_output
+    @target_productivity = @data_case.target_productivity.to_f
+  end
+
+  def filter_params
+    params.permit(:year, :month, :store, :department, :encrypted_password)
   end
 end
