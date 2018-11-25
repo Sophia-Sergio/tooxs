f7e159e6e29c1ca666fdceb219febec209499acf
Sophia-Sergio <san.storres@gmail.com>
2018-11-25 19:02:24 -0300

    chore: change date by default from june to may

--- app/controllers/concerns/demo_parameters.rb
+++ app/controllers/concerns/demo_parameters.rb
@@ -9,9 +9,9 @@ module DemoParameters
       department: 1,
       store:      1,
       search:     '',
-      date:       Date.new(2018, 6, 24),
+      date:       Date.new(2018, 5, 24),
       cluster:    1,
-      month:      6,
+      month:      5,
       year:       2018
     }
   end
