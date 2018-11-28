aed74a1e4faddeb76af3f7723a117242b127bd88
Rodrigo Olave Carrasco <rholave@gmail.com>
2018-11-28 15:11:57 -0300

    chore: add responsive behavior

--- app/assets/javascripts/dashboard-market.js
+++ app/assets/javascripts/dashboard-market.js
@@ -78,7 +78,7 @@
     loop: false,
     margin: 15,
     nav: true,
-    responsive:{ 0:{ items: 1 }, 480:{ items: 2 }, 992:{ items: 2 } }
+    responsive:{ 0:{ items: 1 }, 480:{ items: 1 }, 992:{ items: 2 } }
   });
   // Custom Navigation Events
   $('.dashboard-market__tables__carousel__controls-next').click( function (e) {
