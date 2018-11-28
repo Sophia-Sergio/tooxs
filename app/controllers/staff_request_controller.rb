672146a7eef3ffa3d0c3a8152024b0ffa827bf9f
Rodrigo Olave Carrasco <rholave@gmail.com>
2018-11-28 20:44:28 -0300

    chore: add default parameter to staff request

--- app/controllers/staff_request_controller.rb
+++ app/controllers/staff_request_controller.rb
@@ -4,7 +4,7 @@ class StaffRequestController < ApplicationController
     if params[:month] == nil
       params[:year]  = Date.today.strftime("%Y")
       params[:month] = Date.today.strftime("%m")
-      params[:departments] = []
+      params[:departments] = [1]
       params[:store] = 1
     end
 
@@ -14,8 +14,6 @@ class StaffRequestController < ApplicationController
     @store = params[:store].to_i
     store = Store.find(@store)
 
-    add_breadcrumb "Dashboard", :root_path
-    add_breadcrumb "Dotación personal", :staffing_index_path
   	@search       = ''
     @stores       = Store.all.order(:id)
     @departments  = MasterDepartment.all.order(:id)
@@ -24,7 +22,7 @@ class StaffRequestController < ApplicationController
     @resultadoBusqueda = []
     @resultadoSolicitud = []
 
-    @dep.each do |dep_id|    
+    @dep.each do |dep_id|
       if dep_id == "0"
         @dep = MasterDepartment.all.pluck(:id)
       end
@@ -34,23 +32,23 @@ class StaffRequestController < ApplicationController
 
       # buscar el departamento id
       departament = Department.where(master_id: dep_id, store_id: @store).first
-      if departament 
+      if departament
         dataCase = DataCase.where(dep_num: departament.id, month: @month).first
         if dataCase
-          turnos     = shifts_covered(dataCase.id_case, departament.id , @store)             
+          turnos     = shifts_covered(dataCase.id_case, departament.id , @store)
           turnoCount = 1
-          turnos[:turnosNoCubiertos].each do |cantidad|   
-            if cantidad > 0   
-              month = params[:month].to_i 
+          turnos[:turnosNoCubiertos].each do |cantidad|
+            if cantidad > 0
+              month = params[:month].to_i
               if params[:month].to_i < 10
-               month = "0#{params[:month].to_i}"                            
-              end           
+               month = "0#{params[:month].to_i}"
+              end
               @resultadoBusqueda << {:tienda => store, :dep => departament, :turno => turnoCount, :descrip => "turno #{turnoCount}", :cantidad => cantidad, :fecha => "01-#{month}-#{params[:year]}"}
               turnoCount += 1
-            end         
-          end         
-        end 
-      end     
+            end
+          end
+        end
+      end
       # buscar el caso del departamento
     end
   end
@@ -58,7 +56,7 @@ class StaffRequestController < ApplicationController
   def request_send
     @search = ''
     count_row = params[:count_row].to_i
-    
+
     @carga = []
     (1..count_row).each do |i|
       if params["cargar_#{i}"] == "on"
@@ -66,7 +64,7 @@ class StaffRequestController < ApplicationController
         store = Store.find(params["store_#{i}"].to_i)
         dep = Department.find(params["dep_id_#{i}"].to_i)
         turno = AvailableShift.where(num: params["turno_id_#{i}"].to_i).first
-        @carga << { :store => store, :dep => dep, :turno => turno, :cantidad => params["cantidad_#{i}"], :fecha => params["fecha_#{i}"]}  
+        @carga << { :store => store, :dep => dep, :turno => turno, :cantidad => params["cantidad_#{i}"], :fecha => params["fecha_#{i}"]}
       end
     end
   end
@@ -100,7 +98,7 @@ class StaffRequestController < ApplicationController
     end
 
     (1..count_row).each do |i|
-      RequestDetail.create!(request_id: request.id , department_id: params["dep_#{i}"].to_i, turn: params["turno_#{i}"].to_i, quantity: params["cantidad_#{i}"].to_i, 
+      RequestDetail.create!(request_id: request.id , department_id: params["dep_#{i}"].to_i, turn: params["turno_#{i}"].to_i, quantity: params["cantidad_#{i}"].to_i,
         rrhh: rrhh, internal: pos_interna, laborum: laborum, trabajando: trabajando)
     end
 
@@ -108,7 +106,7 @@ class StaffRequestController < ApplicationController
   end
 
   def show
-      
+
     @request_details = RequestDetail.all
     #@accountUrl = Account.find_by_id(current_account_id)
     #@details = Detail.find_by_acc_id(@accountUrl.id)
