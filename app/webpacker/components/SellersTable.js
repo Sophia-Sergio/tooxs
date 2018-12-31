f740c90e78b34044f712c998e3c00a72a5b1caa4
Rodrigo Olave Carrasco <rholave@gmail.com>
2018-12-31 09:06:47 -0300

    fix: update broken table with bootstrap table 2

--- app/webpacker/components/SellersTable.js
+++ app/webpacker/components/SellersTable.js
@@ -1,53 +1,85 @@
-import React, { Component } from "react"
-import { BootstrapTable, TableHeaderColumn } from 'react-bootstrap-table';
-import DefaultAvatar from '../images/user_default.jpg'
-import PropTypes from "prop-types"
+import React, { Component } from "react";
+import BootstrapTable from 'react-bootstrap-table-next';
+import paginationFactory from 'react-bootstrap-table2-paginator';
+import DefaultAvatar from '../images/user_default.jpg';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
 
 class SellersTable extends Component {
-  state = {
-    tableTitle: 'Resultado de búsqueda',
-    // Ejemplo de Jso
-    employees: [
-      {
-        rut: '12.222.008-7',
-        avatar: '',
-        name: 'Ricardo',
-        lastname: 'Gonzalez',
-        email: 'demo@tooxs.com',
-        phone: '+56999999999',
-        store: 'Alto Las Condes',
-        department: 'Juvenil mujer',
-        shift_description: 'Turno 1 de 45 horas',
-        details: ''
-      }
-    ]
-  }
-
-  // It's a data format example.
-  avatarFormatter(cell, row){
-    console.log(DefaultAvatar);
-    return (cell && cell != '') ? '<div className="thumbnail"><img src=' + cell + ' /></div>' : '<div className="thumbnail"><img src=' + DefaultAvatar + ' /></div>' }
 
   render () {
-    console.log(this.props.sellers);
-    const { tableTitle, employees } = this.state;
+    const Avatar = styled.div`
+      border-radius: 50%;
+      height: 30px;
+      overflow: hidden;
+      width: 30px;
+    `
+    ;
+    const AvatarImg = styled.img`
+      display: block;
+      width: 100%;
+    `
+    ;
+    const sellers = [
+      {
+        id: 1,
+        rut: '12.222.008-7',
+        name: 'Ricardo',
+        lastname: 'Gonzalez'
+      },
+      {
+        id: 2,
+        rut: '14.222.008-7',
+        name: 'Richard',
+        lastname: 'Gonzalez'
+      },
+    ]
+
+    const avatarFormatter = function(cell, row){
+      return <Avatar><AvatarImg src={ cell ? cell : DefaultAvatar }/></Avatar>;
+    };
+
+    const linkFormatter = function(cell, row){
+      return <a href={'/sellers/' + cell}>Detalle</a>;
+    };
+
+    const columns = [
+      {
+        dataField: 'avatar',
+        text: '',
+        formatter: avatarFormatter
+      },
+      {
+        dataField: 'name',
+        text: 'Nombre',
+        sort: true
+      },
+      {
+        dataField: 'lastname',
+        text: 'Apellido',
+        sort: true
+      },
+      {
+        dataField: 'rut',
+        text: 'RUT'
+      },
+      {
+        dataField: 'department_id',
+        text: 'Tienda'
+      },
+      {
+        dataField: 'assigned_shift',
+        text: 'Turno'
+      },
+      {
+        dataField: 'id',
+        text: 'Detalle',
+        formatter: linkFormatter
+      },
+    ];
     return (
       <div className="table-responsive">
-        <BootstrapTable
-          data={this.props.sellers}
-          pagination
-          version='4'
-        >
-          <TableHeaderColumn isKey dataField='avatar'></TableHeaderColumn>
-          <TableHeaderColumn dataField='name' dataSort>Nombre</TableHeaderColumn>
-          <TableHeaderColumn dataField='lastname' dataFormat={this.priceFormatter} dataSort>Apellido</TableHeaderColumn>
-          <TableHeaderColumn dataField='rut' dataSort>RUT</TableHeaderColumn>
-          <TableHeaderColumn dataField='email' dataSort>Email</TableHeaderColumn>
-          <TableHeaderColumn dataField='store_id' dataSort>Tienda</TableHeaderColumn>
-          <TableHeaderColumn dataField='department_id' dataSort>Departamento</TableHeaderColumn>
-          <TableHeaderColumn dataField='assigned_shift' dataSort>Turno</TableHeaderColumn>
-          <TableHeaderColumn dataField='datails'>Más info</TableHeaderColumn>
-        </BootstrapTable>
+        <BootstrapTable bootstrap4 keyField='id' data={ this.props.sellers } columns={ columns } pagination={ paginationFactory() } />
       </div>
     );
   }
