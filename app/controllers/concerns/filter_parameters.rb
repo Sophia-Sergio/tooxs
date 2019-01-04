c6004a90f9306f7a1a22952c2d2ebc3ea2e529e6
Sophia-Sergio <san.storres@gmail.com>
2019-01-04 12:32:53 -0300

    chore: set params dates

--- app/controllers/concerns/filter_parameters.rb
+++ app/controllers/concerns/filter_parameters.rb
@@ -11,4 +11,10 @@ module FilterParameters
   def set_store_department
     @store_dep = StoreDepartment.find_by(store: params[:store], department: params[:department])
   end
+
+  def set_dates
+    @start = Settings.month_start(params[:year_start], params[:month_start])
+    @end = Settings.month_end(params[:year_end] || params[:year_start],
+                              params[:month_end] || params[:month_start])
+  end
 end
