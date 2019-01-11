142e4ce53a711c38d5e028740b4c39ad84217cf2
Sophia-Sergio <san.storres@gmail.com>
2019-01-11 20:47:27 -0300

    chore: sellers last_name

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -16,10 +16,11 @@ module Api
         goals = goals(plan_hours, params[:year_start], params[:month_start])
         shifts = Seller.where(id: sellers.ids).shifts_ids(params[:year_start], params[:month_start])
 
-        sellers_table = sellers.each_with_object([]) do |seller, array|
+        data_table = sellers.each_with_object([]) do |seller, array|
           array << {
             id: seller.id,
             name: seller.name,
+            last_name: seller.surname_1,
             avatar: '',
             shifts: shifts[seller.id].join(','),
             sell: achievements[seller.id],
@@ -28,7 +29,7 @@ module Api
             link: seller_path(seller)
           }
         end
-        render json: sellers_table
+        render json: data_table
       end
 
       def goals(plan_hours, year, month)
