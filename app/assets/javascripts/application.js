76e53f93191198d005a445087ecae164e2696bcc
Rodrigo Olave Carrasco <rholave@gmail.com>
2019-01-16 21:08:09 -0300

    chore: remove unused assets

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,55 +10,5 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
-// require turbolinks
-
-//= require plugins/pace/pace.min.js
-//= require plugins/jquery/jquery-1.11.1.min.js
-//= require plugins/modernizr.custom.js
-//= require plugins/jquery-ui/jquery-ui.min.js
-//= require plugins/tether/js/tether.min.js
-//= require plugins/bootstrap/js/bootstrap.min.js
-//= require plugins/jquery/jquery-easy.js
-//= require plugins/jquery-unveil/jquery.unveil.min.js
-//= require plugins/jquery-bez/jquery.bez.min.js
-//= require plugins/jquery-ios-list/jquery.ioslist.min.js
-//= require plugins/imagesloaded/imagesloaded.pkgd.min.js
-//= require plugins/jquery-actual/jquery.actual.min.js
-//= require plugins/jquery-scrollbar/jquery.scrollbar.min.js
-
-//= require jquery-datatable/media/js/jquery.dataTables.min.js
-//= require jquery-datatable/extensions/TableTools/js/dataTables.tableTools.min.js
-//= require jquery-datatable/media/js/dataTables.bootstrap.js
-//= require jquery-datatable/extensions/Bootstrap/jquery-datatable-bootstrap.js
-//= require datatables-responsive/js/datatables.responsive.js
-//= require datatables-responsive/js/lodash.min.js
-
-//= require plugins/interactjs/interact.min.js
-//= require plugins/moment/moment-with-locales.min.js
-//= require plugins/select2/js/select2.full.min.js
-//= require plugins/classie/classie.js
-//= require plugins/switchery/js/switchery.js
-
-//= require pages/js/pages.js
-//= require pages/js/pages.calendar.js
-
-//= require js/bootstrap-datepicker.min
-
-//= require js/calendar.js
-//= require js/scripts.js
-//= require js/chart.min.js
-//= require mta/js/Cerebro.js
-//= require mta/js/mta-custom.js
-//= require mta/js/api.js
-//= require mta/js/Calculo.js
-//= require mta/js/SalesCluster.js
-//= require mta/js/ProductivityCluster.js
-//= require mta/js/ProductivityReport.js
-//= require mta/js/HourAnalysis.js
-//= require mta/js/EfficiencyReport.js
-//= require mta/js/app.js
-//= require owl.carousel.min
-//= require js/jquery.maskMoney.min
 //= require_tree .
 //= require_self
