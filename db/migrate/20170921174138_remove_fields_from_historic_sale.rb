77b69c56cfd8e150ea221b044609ecc9def207d0
Sophia-Sergio <san.storres@gmail.com>
2019-01-19 21:45:40 -0300

    refactor: delete unused files

--- db/migrate/20170921174138_remove_fields_from_historic_sale.rb
+++
@@ -1,7 +0,0 @@
-class RemoveFieldsFromHistoricSale < ActiveRecord::Migration[5.1]
-  def change
-    remove_column :historic_sales, :month, :integer
-    remove_column :historic_sales, :week, :integer
-    remove_column :historic_sales, :day, :string
-  end
-end
--- db/migrate/20180501204206_add_productivity_obj_to_store_departments.rb
+++
@@ -1,5 +0,0 @@
-class AddProductivityObjToStoreDepartments < ActiveRecord::Migration[5.1]
-  def change
-    add_column :store_departments, :productivity_obj, :float
-  end
-end
