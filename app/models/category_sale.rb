b1e53013748a3a5bb359c7766b13b8ced5d6dd0b
Sophia-Sergio <san.storres@gmail.com>
2019-01-28 18:26:35 -0300

    refactor: left only working-code

--- app/models/category_sale.rb
+++ app/models/category_sale.rb
@@ -1,4 +1,11 @@
 class CategorySale < ApplicationRecord
-  belongs_to :category
+  belongs_to :category, foreign_key: 'category_cod'
   belongs_to :store
+
+  scope :by_cluster, ->(cluster = nil) { where(cluster_id: cluster).order(:name) }
+  scope :between, ->(start_date, end_date) { where('date between ? AND ?', start_date, end_date) }
+
+  def self.total
+    sum(:amount)
+  end
 end
--- app/models/target_productivity.rb
+++ app/models/target_productivity.rb
@@ -7,6 +7,16 @@ class TargetProductivity < ApplicationRecord
     saturday_sunday_pm: 4
   }
 
+  scope :between_dates, ->(start_date, end_date) {
+    if start_date.year == end_date.year
+      where('year = ? AND month >= ? AND month <= ?',
+        start_date.year, start_date.month, end_date.month)
+    else
+      where('year = ? AND month >= ?', start_date.year, start_date.month).
+      or(where('year = ? AND month <= ?', end_date.year, end_date.month))
+    end
+  }
+
   def self.by_date(date)
     find_by(Settings.config_date(date)).amount
   end
@@ -17,5 +27,24 @@ class TargetProductivity < ApplicationRecord
       hash[array[0]] ||= {}
       hash[array[0]][array[1]] = array[2]
     end
+  end
+
+  def self.by_date_hour(period)
+    hash = between_dates(period[:start], period[:end]).
+      order(:year, :month, :week).uniq.each_with_object({}) do |prod, h|
+        week_period = Settings.week_period(prod.year, prod.month, prod.week)
+        (week_period[:start]..week_period[:end]).each do |date|
+          next unless date_period(date, prod.period)
+
+          (h[date] ||= []) << Settings.week_periods_keys[prod.period].map { |k| [k => prod.amount] }
+        end
+      end
+    hash = hash.extract!(*(period[:start]..period[:end]))
+    hash.keys.each_with_object({}) { |k, h| h[k] = hash[k].flatten.reduce(:merge) }
+  end
+
+  def self.date_period(date, period)
+    ((1..5).to_a.include?(date.wday) && self.periods.keys[0..1].include?(period)) ||
+    ([6,0].include?(date.wday) && self.periods.keys[2..3].include?(period))
   end
 end
