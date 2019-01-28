e8c29cd7c512511f3d8b5dd3be7d1ad815918f16
Sophia-Sergio <san.storres@gmail.com>
2019-01-28 17:35:29 -0300

    refactor: left only working code

--- app/controllers/concerns/filter_parameters.rb
+++ app/controllers/concerns/filter_parameters.rb
@@ -4,18 +4,8 @@
 module FilterParameters
   extend ActiveSupport::Concern
 
-  def set_stores
-    @stores = Store.by_cluster(params[:cluster])
-  end
-
   def set_store_department
     @store_dep = StoreDepartment.find_by(
       store: params[:store], department: params[:department])
-  end
-
-  def set_dates
-    @start = Settings.month_start(params[:year_start], params[:month_start])
-    @end = Settings.month_end(params[:year_end] || params[:year_start],
-                              params[:month_end] || params[:month_start])
   end
 end
