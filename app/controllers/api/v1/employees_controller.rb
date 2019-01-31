f3543eb076d7c57e1fa3bdc4b62ea572fc7b7f76
Sophia-Sergio <san.storres@gmail.com>
2019-01-31 19:57:24 -0300

    chore: add return

--- app/controllers/api/v1/employees_controller.rb
+++ app/controllers/api/v1/employees_controller.rb
@@ -9,10 +9,14 @@ module Api
       skip_before_action :verify_authenticity_token
 
       def sales_assistants_table
+        return nil unless @store_dep.sellers.any?
+
         @store_dep.sales_asisstants.as_json(only: [:id, :name, :surname_1])
       end
 
       def sellers_table
+        return nil unless @store_dep.sellers.any?
+
         sellers = @store_dep.sellers.working_on_period(@period)
         achievements = sellers.total_achievements(@period)
         plan_hours = sellers.plan_hours(params[:year_start], params[:month_start])
@@ -33,6 +37,22 @@ module Api
           }
         end
       end
+
+      "hola":{
+        "first":
+          {
+            "name":"CUMPLIMIENTO DE EFICIENCIA",
+            "value":32.08857142857143,
+            "description":"Eficiencia Objetivo 75%"},
+        "second":
+          {
+            "name":"CUMPLIMIENTO PLAN DE VENTA",
+            "value":null,
+            "description":"Plan $9725163280.0. Real $347327260.0"
+          },
+        "third":
+          144603.29719858157}
+
 
       def goals(plan_hours, year, month)
         periods = TargetProductivity.periods.keys
