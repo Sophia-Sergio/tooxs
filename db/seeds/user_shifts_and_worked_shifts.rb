b802bb82319aed9e67e62c548a928d5afe9f63c7
Sophia-Sergio <san.storres@gmail.com>
2019-01-31 13:30:11 -0300

    hotfix: seed worked shift

--- db/seeds/user_shifts_and_worked_shifts.rb
+++ db/seeds/user_shifts_and_worked_shifts.rb
@@ -1,3 +1,6 @@
+UserShift.where(year: [2018, 2019]).delete_all
+WorkedShift.where('date >= ?', Date.new(2018, 1, 1)).delete_all
+
 users = User.employees
 (2018..2019).each do |year|
   users.each do |user|
@@ -5,7 +8,7 @@ users = User.employees
     date = Date.new(year, 1, 1) if year == 2018
     date = Date.new(2018, 12, 31) if year == 2019
 
-    break if date > Date.new(2019, 3, 1)
+    break if date Date.new(2019, 3, 1)
 
     (1..12).each do |month|
       work_shift = WorkShift.all.sample
@@ -26,12 +29,4 @@ users = User.employees
     end
   end
   puts "user_shifts_and_worked_shifts #{year} added"
-end
-
-
-
-(1..15).each_with_index do |number, index|
-  puts "hola #{index}"
-  break if index > 12
-
 end
