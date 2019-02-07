542d042d598708e3787d2187dec8901d7def2b42
Rodrigo Olave Carrasco <rholave@gmail.com>
2019-02-07 16:45:45 -0300

    chore: format chart data label

--- app/webpacker/components/SalesMonth.js
+++ app/webpacker/components/SalesMonth.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import axios from 'axios';
 import { currencyFormat, monthFormat, dayMonthFormat } from './helpers';
-import { currencyFormat, monthFormat } from './helpers';
 import Loader from "./layout/Loader";
 import Select from 'react-select';
 import MonthPicker from './shared/MonthPicker';
@@ -97,6 +96,12 @@ class SalesMonth extends Component {
           chartTitle: 'Gráfico de ventas',
           loading: false
         });
+        this.setState({
+          chartData: {
+            ...this.state.chartData,
+            labels: this.state.chartData.labels.map( label => ( dayMonthFormat(label) ) )
+          }
+        });
         this.setState(state => {
           state.chartData.datasets[0].backgroundColor = 'rgba(71, 196, 254, 0)';
           state.chartData.datasets[0].borderColor = 'rgba(71, 196, 254, 1)';
@@ -139,6 +144,12 @@ class SalesMonth extends Component {
           chartData: res.data,
           chartTitle: 'Gráfico comparativo de ventas',
           loading: false
+        });
+        this.setState({
+          chartData: {
+            ...this.state.chartData,
+            labels: this.state.chartData.labels.map( label => ( dayMonthFormat(label) ) )
+          }
         });
         this.setState(state => {
           state.chartData.datasets[0].backgroundColor = 'rgba(71, 196, 254, 0)';
