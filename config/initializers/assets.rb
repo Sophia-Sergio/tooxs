ec0579ab799025ee28d8d5a854b60a0084e612de
Sophia-Sergio <san.storres@gmail.com>
2019-02-07 15:05:25 -0300

    refactor: remove assets

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,13 +7,6 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'plugins')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'assets', 'js')
-Rails.application.config.assets.paths << Rails.root.join('vendor')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'pages')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'owlcarousel')
-Rails.application.config.assets.paths << Rails.root.join('vendor', 'owlcarousel', 'assets')
 Rails.application.config.assets.precompile += %w[*scss *.svg *.eot *.woff *.ttf *.woff2]
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
