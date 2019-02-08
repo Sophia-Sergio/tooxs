34467690a8490fae24252eb8a078dd43e9b38ec9
Sophia-Sergio <san.storres@gmail.com>
2019-02-08 15:26:23 -0300

    refactor: add memoization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,11 +2,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!, :set_current_user_store_department
 
-
   private
 
   def set_current_user_store_department
-    if current_user&.store && !current_user.store_department
+    if current_user&.store && current_user.store_department_id.nil?
       world = current_user.store.bigger_plan_sale_world
       department = world.bigger_plan_sale_department(current_user.store)
       current_user.store_department = StoreDepartment.find_by(
