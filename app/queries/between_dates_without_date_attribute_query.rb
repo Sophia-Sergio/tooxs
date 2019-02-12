4466fb27fe69fc0aaf16a23642ad741e5d34f75e
Sophia-Sergio <san.storres@gmail.com>
2019-02-12 12:03:33 -0300

    chore: move query to query object

---
+++ app/queries/between_dates_without_date_attribute_query.rb
@@ -0,0 +1,19 @@
+class BetweenDatesWithoutDateAttributeQuery
+  include CommercialCalendar::Period
+
+  def initialize(model, period)
+    @model = model
+    @period = period
+  end
+
+  def call
+    if year_by_date(@period[:start]) == year_by_date(@period[:end])
+      @model.order(:year, :month, :week).where('year = ? AND month >= ? AND month <= ?',
+        year_by_date(@period[:start]), month_by_date(@period[:start]), month_by_date(@period[:end]))
+    else
+      @model.order(:year, :month, :week).
+        where('year = ? AND month >= ?', year_by_date(@period[:start]), month_by_date(@period[:start])).
+        or(where('year = ? AND month <= ?', year_by_date(@period[:end]), month_by_date(@period[:end])))
+    end
+  end
+end
