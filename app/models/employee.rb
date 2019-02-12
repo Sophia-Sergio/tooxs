08b97d3d984daadca738544c65346abe64d98d57
Sophia-Sergio <san.storres@gmail.com>
2019-02-12 11:42:09 -0300

    chore: add method to provide the shifts by period

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -4,6 +4,7 @@ class Employee < User
   belongs_to :store_department
   delegate :department, to: :store_department, allow_nil: true
   delegate :store, to: :store_department, allow_nil: true
+  has_many :work_shifts, through: :shifts
 
   default_scope { active.joins(:roles).where.not(roles: { name: ['admin'] }) }
 
@@ -14,6 +15,36 @@ class Employee < User
   scope :working_on_period, ->(period) {
     joins(:worked_shifts).where(worked_shifts: { date: period[:start]..period[:end] }).distinct
   }
+
+  def calendar_shift(period)
+    worked_shift_period = { start: period[:start], end: Date.today - 1 }
+    plan_shift_period   = { start: Date.today, end: period[:end] }
+    worked_shift(worked_shift_period).merge(plan_shift(plan_shift_period))
+  end
+
+  def worked_shift(period)
+    hash = worked_shifts.between_dates(period).each_with_object({}) do |worked_shift, h|
+      h[worked_shift.date] = "#{worked_shift.check_in.strftime('%H:%M')}-#{worked_shift.check_out.strftime('%H:%M')}"
+    end
+    (period[:start]..period[:end]).each_with_object({}) { |d, h| h[d] = nil }.merge(hash)
+  end
+
+  def plan_shift(period)
+    date = period[:start]
+    hash = shifts.between_dates(period).each_with_object({}) do |shift, h|
+      opts = { year: shift.year, month: shift.month, week: shift.week, shift: shift.work_shift_id }
+      PlanShift.where(work_shift_id: opts[:shift], week: opts[:week]).
+        order(:week, :day).each do |plan_shift|
+          next unless (period[:start]..period[:end]).cover? date_by_params(opts.merge(day: plan_shift.day))
+
+          check_in   = plan_shift[:check_in]&.strftime('%H:%M')
+          check_out  = plan_shift[:check_out]&.strftime('%H:%M')
+          h[date] = "#{check_in}-#{check_out}" if check_in.present? && check_out.present?
+          date += 1
+        end
+    end
+    (period[:start]..period[:end]).each_with_object({}) { |d, h| h[d] = nil }.merge(hash)
+  end
 
   def self.employees_by_hour(date)
     DateStaffQuery.new.call(date).each_with_object({}) do |employee, hash|
